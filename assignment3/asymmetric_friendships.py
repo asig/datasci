4b30e3a86c8a01210f715b9093b2b45f61b6b3d6
Andreas <andreas@fractal-labs.com>
2014-07-16 18:55:54 +0200

    cleaner 'hack'. stupid python

--- assignment3/asymmetric_friendships.py
+++ assignment3/asymmetric_friendships.py
@@ -20,24 +20,9 @@ def reducer(key, list_of_values):
     total = 0
     for v in list_of_values:
         total += v
-    if v < 0:
-        # "key" was expected to be seen, but it didn't show up. report it.
-
-        # this is a hack to make the grader accept the solution... Totally fucked up!
-        # we should just emit "key"
-        a = key[0]
-        b = key[1]
-        skip = (a == "Champtercier" and b == "Myriel") or \
-               (a == "Myriel" and b == "Champtercier") or \
-               (a == "Myriel" and b == "Valjean") or \
-               (a == "Valjean" and b == "Myriel")
-        if not skip:
-            mr.emit ( key )
-        a = key[1]
-        b = key[0]
-        skip = (a == "Champtercier" and b == "Myriel") or (a == "Myriel" and b == "Champtercier") or (a == "Myriel" and b == "Valjean") or (a == "Valjean" and b == "Myriel")
-        if not skip:
-            mr.emit ( (a, b) )
+    if total < 0:
+        mr.emit ( key )
+        mr.emit ( (key[1], key[0]) )
 
 
 
